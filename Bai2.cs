d7c4f7f96232ec985d4fa33c09ce94899e7184b1
lehuyhieu1099 <66100506+lehuyhieu1099@users.noreply.github.com>
2020-07-17 15:01:29 +0700

    Bai tap 17/7/2020

---
+++ Bai2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_and_Method
+{
+    class Bai2
+    {
+        public int[,] CreateMatrix1(int side)
+        {
+            int[,] matrix = new int[side, side];
+            Random rnd = new Random();
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    matrix[i, j] = rnd.Next(10, 40);
+                }
+            }
+            return matrix;
+        }
+        public int[,] CreateMatrix2(int side)
+        {
+            int[,] matrix = new int[side, side];
+            Random rnd = new Random();
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    matrix[i, j] = rnd.Next(10, 40);
+                }
+            }
+            return matrix;
+        }
+        //Gọi C là mảng chứa tổng của hai mảng trên
+        public int[,] Sumof2Matrix(int[,] A, int[,] B)
+        {
+            int n = A.GetLength(0);
+            int[,] C = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                }
+            }
+            return C;
+        }
+        //Gọi D là mảng chứa tích của hai mảng trên
+        public int[,] MultiOf2Matrix(int[,] A, int[,] B)
+        {
+            int n = A.GetLength(0);
+            int[,] D = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int k = 0; k < n; k++)
+                {
+                    D[i, k] = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        D[i, k] +=  A[i, j] * B[j, k];
+                    }
+                }
+            return D;
+        }
+        public void ShowMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Array_and_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bài 1:
+            //Bai1 bai1 = new Bai1();
+
+            //Console.WriteLine("Bai1: \n");
+            //var matrix = bai1.CreateMatrix(10);
+            //bai1.ShowMatrix(matrix);
+            //Console.WriteLine("Tong cac so chan: " + bai1.SumOfEvenNumber(matrix));
+            //Console.WriteLine("Tong duong cheo chinh: " + bai1.SumOfMainDiagonal(matrix));
+            //Console.WriteLine("Tong duong cheo phu: " + bai1.SumOfExtraDiagonal(matrix));
+            //Console.WriteLine("Tong duong bien: " + bai1.SumOfBorderLine(matrix));
+            //bai1.CreatUnderTritangle(matrix);
+            //Console.WriteLine();
+            //bai1.CreatAboveTritangle(matrix);
+
+
+            //Console.WriteLine("Bai2: \n");
+
+            //Bai2 bai2 = new Bai2();
+            //var matrix1 = bai2.CreateMatrix1(3);
+            //var matrix2 = bai2.CreateMatrix2(3);
+            //bai2.ShowMatrix(matrix1);
+            //Console.WriteLine();
+            //bai2.ShowMatrix(matrix2);
+            //Console.WriteLine();
+            //var summatrix = bai2.Sumof2Matrix(matrix1, matrix2);
+            //bai2.ShowMatrix(summatrix);
+            //Console.WriteLine();
+            //var multimatrix = bai2.MultiOf2Matrix(matrix1, matrix2);
+            //bai2.ShowMatrix(multimatrix);
+
+            //Bài 3:
+            Console.WriteLine("Bai3: \n");
+            Bai3 bai3 = new Bai3();
+            var matrix3 = bai3.CreateMatrix(5, 4);
+            bai3.ShowMatrix(matrix3);
+            //Console.WriteLine("Enter V: ");
+            //int v = int.Parse(Console.ReadLine());
+            //var vmatrix = bai3.Findv(v, matrix3);
+            //bai3.ShowMatrix(vmatrix);
+            //var matrixof5 = bai3.FindMultiplesOf5(matrix3);
+            //bai3.ShowMatrix(matrixof5);
+            Console.WriteLine();
+            var matrix_3 = bai3.CreateMatrix2(matrix3);
+            bai3.ShowMatrix(matrix_3);
+
+
+
+
+
+        }
+    }
+}
